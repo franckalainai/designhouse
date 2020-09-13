94dadf55c63dd80f105e3b9f1c863f41f147e907
franckalainai <franckalain.ai@gmail.com>
2020-09-13 22:48:25 +0000

    password email

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 // Public Routes
+Route::get('me', 'User\MeController@Me');
+
 // Route group for authenticated uses only
 Route::group(['middleware' => ['auth:api']], function(){
-
+    Route::post('logout', 'Auth\LoginController@logout');
 });
 
 // Routes for guest only
@@ -13,4 +15,6 @@ Route::group(['middleware' => ['guest:api']], function(){
     Route::post('verification/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
     Route::post('verification/resend', 'Auth\VerificationController@resend');
     Route::post('login', 'Auth\LoginController@login');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 });
